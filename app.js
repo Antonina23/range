05e14a97ebe9e43bd2250af7c8017a85afe748d2
Antonina23 <tone4kawow@gmail.com>
2019-04-08 22:32:29 +0300

    Fix app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 function range (start, end) {
 	if (typeof(start) == "number" && typeof(end) == "number") {
+		if (end < start) {
+			console.log("Wrong range");
+		} else {
 		for(var i = start; i <= end; i++) {
 			console.log(i);
+			}
 		}
 	} else {
 		console.log("Please, type a number");
